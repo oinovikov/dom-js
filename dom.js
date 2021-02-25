f0e8f5233401dbf57e2c2b5f5b63fa7a0f52c455
Oleg Novikov <olegn@outlook.com>
2021-02-25 18:07:36 +0300

    Init

---
+++ dom.js
@@ -0,0 +1,12 @@
+;(
+    /**
+     * @param {window} GLOBAL
+     * @param {undefined} undefined
+     */
+    function (GLOBAL, undefined) {
+        var DOC = GLOBAL.document;
+        var $root = DOC.documentElement;
+        var $head = DOC.head;
+        var $body;
+    }
+)(new Function('return this')());
